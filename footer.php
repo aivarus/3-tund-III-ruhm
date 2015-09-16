0169aa837bb641993e7d4e0c3da2424d49c77969
Aivar Russki <aivarus@greeny.cs.tlu.ee>
2015-09-16 11:23:11 +0300

    implented header and footer

---
+++ footer.php
@@ -0,0 +1,3 @@
+	<p><i>Lehe tegi Aivar, 2015a<?p><?i>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,12 @@
+
+<?php
+	//tõstame teise faili sisu siia
+	require_once("menu.php");
+	
+?>
+
+<html>
+<head>
+	<title>Login page</title>
+</head>
+<body>
